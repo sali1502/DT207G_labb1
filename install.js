7392c047991119ae4a32aaf3e5b65b45b890ec5c
sali1502 <sali1502@student.miun.se>
2024-06-01 16:41:33 +0200

    Install-script för SQLite3

---
+++ install.js
@@ -0,0 +1,22 @@
+/* Install-script för labb 1, DT207G Backend-baserad webbutveckling. Åsa Lindskog, sali1502@student.miun.se */
+
+const sqlite3 = require("sqlite3").verbose();
+
+//Skapa databas
+const db = new sqlite3.Database("./db/courses.db");
+
+//Skapa tabell (id, coursecode, coursename, syllabus, progression)
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS courses;");
+    db.run(`
+    CREATE TABLE courses(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    coursecode TEXT NOT NULL,
+    coursename TEXT NOT NULL,
+    syllabus TEXT NOT NULL,
+    progression TEXT NOT NULL
+    );
+`);
+});
+
+db.close();
